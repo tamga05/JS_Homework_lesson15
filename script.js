edd4b0cf8e70c09800fbe33a3fed9de1d2763286
Andrey Nikitin <81686981+tamga05@users.noreply.github.com>
2021-12-21 23:08:17 +0300

    tasks solving

--- script.js
+++ script.js
@@ -1,14 +1,57 @@
+"use strict";
+
 // 1
-let admin
-let name
+let admin;
+let name;
+//let age = 49;
 
 // 2
-name = 'Джон'
+name = "Джон";
 
 // 3
-admin = name
+admin = name;
 
 // 4
-console.log(admin)
+console.log(admin);
+//console.log('Имя администратора ' + admin + ', ему ' + age + ' лет.');
 
 // 5
+let house = {
+  city: "Moscow",
+  address: "Pushkinskaya street",
+};
+
+// console.log(object);
+
+// 7
+let user = {};
+
+// 8
+user.name = "John";
+console.log(user);
+
+// 9
+user.surname = "Smith";
+console.log(user);
+
+user.name = "Pete";
+console.log(user.name);
+
+
+
+
+
+let str = `a
+b
+c`;
+console.log(str);
+
+console.log("true" * 7);
+
+console.log(10 / 0);
+
+console.log(-10 * 0);
+
+console.log(10 / -0);
+
+document.write(admin);
